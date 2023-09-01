4fa19aa84e1ce52cac6b4c2f3f0de1938f4cc4ee
KingJiwon <wldnjs0401@naver.com>
2023-09-01 17:36:01 +0900

    chore: eslint, prettier,scss 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './styles/App.scss';
 
+const jiwon = 'jiwon';
 function App(): JSX.Element {
 	return (
 		<div className="App">
+			<header>{jiwon}</header>
 			<p>hi</p>
 		</div>
 	);
