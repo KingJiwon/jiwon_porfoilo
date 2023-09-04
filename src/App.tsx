840e0b0062144ada89f9e6099f6c4a2512ca6e05
KingJiwon <wldnjs0401@naver.com>
2023-09-04 23:10:12 +0900

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './styles/App.scss';
 
-const jiwon = 'jiwon';
 function App(): JSX.Element {
 	return (
 		<div className="App">
-			<header>{jiwon}</header>
 			<p>hi</p>
 		</div>
 	);
